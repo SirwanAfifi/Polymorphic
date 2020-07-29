8466b9304d8db6b8c80e24d8f16850a629ed0c92
Sirwan <sir1afifi@gmail.com>
2020-07-29 21:31:48 +0430

    Add solution 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Polymorphic.Data;
+using Polymorphic.Models;
 
 namespace Polymorphic
 {
@@ -14,8 +19,33 @@ namespace Polymorphic
             var dbContext = serviceProvider.GetService<MyDbContext>();
 
             var faker = new Bogus.Faker();
+
+            var article = new Article
+            {
+                Title = "Article A",
+                Slug = "article_a",
+                Description = "No Description"
+            };
+            var comment = new Comment
+            {
+                CommentText = "It's great",
+                User = "Sirwan"
+            };
+            dbContext.ArticleComments.Add(new ArticleComment
+            {
+                Article = article,
+                Comment = comment
+            });
+
+            dbContext.SaveChanges();
             
-            
+            var articleOne = dbContext.Articles
+                .Include(article => article.ArticleComments)
+                .ThenInclude(comment => comment.Comment)
+                .First(article => article.Id == 1);
+            var article1Comments = articleOne.ArticleComments.Select(x => x.Comment);
+            Console.WriteLine(article1Comments.Count());
+
         }
         private static void ConfigureServices(IServiceCollection services)
         {
